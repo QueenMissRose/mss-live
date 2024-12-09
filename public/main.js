b02275b723e00be748bcf0a19aba47090edb2c50
jolyssa <jolyssa.armstrong@gmail.com>
2024-12-09 04:38:53 -0500

    broke the contact button, and more styles

--- public/main.js
+++ public/main.js
@@ -1,3 +1,1 @@
-document.querySelector('.contact').addEventListener('click', function () {
-    window.open('https://calendly.com/mojistaticstudios', '_blank')
-})
+let scroll = () => document.getElementById('contact').scrollIntoView()
