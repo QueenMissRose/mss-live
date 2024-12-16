f48da05cf04f4be43715d38b33d49ecb7072e47b
jolyssa <jolyssa.armstrong@gmail.com>
2024-12-16 18:32:43 -0500

    remove gsap

--- public/main.js
+++ public/main.js
@@ -1,12 +1,12 @@
 //for now
-gsap.from('.because', {
-    duration: 1,
-    x: '-100vw',
-    ease: 'power.in'
-  })
+// gsap.from('.because', {
+//     duration: 1,
+//     x: '-100vw',
+//     ease: 'power.in'
+//   })
   
-gsap.from('.logo', {
-    duration: 1,
-    delay: 1,
-    x: '-500%'
-  })
+// gsap.from('.logo', {
+//     duration: 1,
+//     delay: 1,
+//     x: '-500%'
+//   })
