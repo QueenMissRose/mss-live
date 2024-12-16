f8cfde3e654cb527af54dc3be99067ca49457ab6
jolyssa <jolyssa.armstrong@gmail.com>
2024-12-15 23:29:40 -0500

    hosting on cdn, pls work.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     extend: {
       fontFamily: {
         roboto: ['Roboto Mono', 'mono'],
-        milk: ['milk', 'DM Serif Text', 'serif'],
+        milk: ['milk', 'serif'],
       },
       boxShadow: {
         'boxy': '4px 4px 0px 0px rgba(0, 0, 0, 25%)',
